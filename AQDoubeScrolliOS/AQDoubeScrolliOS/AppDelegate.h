772a597a7284672a60b7a236df8b61b10a7472d3
Alessio Quattrocchi <alessio.quattrocchi@nextrek.net>
2016-11-08 17:37:54 +0100

    first commit

---
+++ AQDoubeScrolliOS/AQDoubeScrolliOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AQDoubeScrolliOS
+//
+//  Created by Alessio on 08/11/16.
+//  Copyright © 2016 Alessio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AQDoubeScrolliOS/AQDoubeScrolliOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AQDoubeScrolliOS
+//
+//  Created by Alessio on 08/11/16.
+//  Copyright © 2016 Alessio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
